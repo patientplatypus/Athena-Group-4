0bfd04f1fa8c3c6668b979f31505bd421c491a1d
Jesse Kipp <jtkipp@gmail.com>
2017-04-29 21:27:36 -0500

    ci Add ngRoute

--- client/patient/index.js
+++ client/patient/index.js
@@ -1,5 +1,15 @@
 var patientHomePage = require('./patientHomePage/patientHomePage')
 
 var patientApp = angular.module('patientApp', [
+  'ngRoute',
   patientHomePage
 ]);
+
+patientApp.config(['$locationProvider', '$routeProvider',
+  function($locationProvider, $routeProvider) {
+    $locationProvider.hashPrefix('!');
+    $routeProvider.when('/', {
+      template: '<patient-home-page></patient-home-page>'
+    });
+}]);
+
