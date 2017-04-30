04b7de13ff503d96ce9065ff3dc64fbc858923a3
patientplatypus <pweyand@gmail.com>
2017-04-29 20:07:38 -0500

    local changes

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
 	$("#submit").on('click',function() {
 		//set local storage, manage access
 		populateStorage();
+		console.log('onclick works')
 		window.location.replace("./" + $("#selector").value + ".ejs")
 	});
 
